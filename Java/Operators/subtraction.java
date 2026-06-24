58bbf75076b0f0992a4803c982e997c4a7eccafc
sneha2004-debug <snehakadamsk6@gmail.com>
2026-06-24 22:20:22 +0530

    index.html
    css task 1 added

---
+++ Java/Operators/subtraction.java
@@ -0,0 +1,48 @@
+class subtraction{
+	public static void main(String[] args)
+	{	//char
+		char c1='a';
+		char c2='b';
+		int c3=c1-c2;
+		System.out.println("Char Output"+c3);
+
+		//byte
+		byte b1='a';
+		byte b2='b';
+		int b3=b1-b2;
+		System.out.println("Byte Output"+b3);
+
+		//int
+		int i1='a';
+		int i2='b';
+		int i3 =i1-i2;
+		System.out.println("Int Output"+i3);
+
+		//short
+		short s1='a';
+		short s2='b';
+		int s3 =s1-s2;
+		System.out.println("Short Output"+s3);
+
+		//float
+		float f1='a';
+		float f2='b';
+		float f3 =f1-f2;
+		System.out.println("Float output"+f3);
+
+		//double
+		double d1='a';
+		double d2='b';
+		double d3 =d1-d2;
+		System.out.println("Double output"+d3);
+
+		// //boolean
+		// boolean bl1=true;
+		// boolean bl2=false;
+		// boolean bl3 =bl1-bl2;
+		// System.out.println("boolean"+bl3);
+
+
+
+	}
+}
