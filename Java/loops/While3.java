7366e420ac9b6767a34f09bf4e65b7fba3da18c2
sneha2004-debug <snehakadamsk6@gmail.com>
2026-07-14 19:30:35 +0530

    while loop
    printing characters in normal and reverse order

---
+++ Java/loops/While3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class While3{
+    public static void main(String args[]){
+        char a='a';
+        while(a<='z')
+        {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
---
+++ Java/loops/While4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class While4{
+    public static void main(String args[])
+    {
+        char z='z';
+        while(z>='a')
+        {
+            System.out.println(z);
+            z--;
+        }
+    }
+}
