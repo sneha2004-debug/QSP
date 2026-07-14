e98a53ecb5c40665bbea495545422282683e1ebd
sneha2004-debug <snehakadamsk6@gmail.com>
2026-07-14 19:39:22 +0530

    leap year
    findingleap year with while loop code added

---
+++ Java/loops/While5.java
@@ -0,0 +1,15 @@
+class While5{
+    public static void main(String args[])
+    {
+        int year=2000;
+        while(year<=3000)
+        {
+            if(year%4==0 && year%100!=0 || year%400==0)
+            {
+                System.out.println(year);
+
+            }
+            year++;
+        }
+    }
+}
