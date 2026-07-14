09cedd6e74c86ba31b2cb5ea41f995477b2bc3de
sneha2004-debug <snehakadamsk6@gmail.com>
2026-07-14 19:56:46 +0530

    Do while loop
    printing numbers with do while loop

---
+++ Java/loops/Do-While/DoWhile1.java
@@ -0,0 +1,11 @@
+class DoWhile1
+{
+    public static void main(String args[])
+    {
+        int a=0;
+        do{
+            System.out.println("Hi");
+            a++;
+        }while(a<5);
+    }
+}
---
+++ Java/loops/Do-While/DoWhile2.java
@@ -0,0 +1,10 @@
+class DoWhile2{
+    public static void main(String args[])
+    {
+        int a=1;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<=100);
+    }
+}
