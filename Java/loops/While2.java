8ffaaba027bfcc5dee6b0efa387df239c2d15388
sneha2004-debug <snehakadamsk6@gmail.com>
2026-07-14 19:25:12 +0530

    while loop
    printing 100 numbers with while loop

---
+++ Java/loops/While2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class While2{
+    public static void main(String args[])
+    {
+        int a=1;
+        while(a<=100)
+        {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
