70a605252786b4c2bff11c7501eb07c4262ecb6b
sneha2004-debug <snehakadamsk6@gmail.com>
2026-07-14 20:00:49 +0530

    printing character in loop
    do while loop

---
+++ Java/loops/Do-While/DoWhile3.java
@@ -0,0 +1,10 @@
+class DoWhile3{
+    public static void main(String args[])
+    {
+        char start='a';
+        do{
+            System.out.println(start);
+            start++;
+        }while(start<='z');
+    }
+}
