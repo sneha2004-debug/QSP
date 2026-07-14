83dbb929d66cfee1e8aac3884cc398db99b24fb6
sneha2004-debug <snehakadamsk6@gmail.com>
2026-07-14 19:22:47 +0530

    whileloop added

---
+++ Java/loops/while/While1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class While1{
+    public static void main(String args[])
+    {
+        int a=0;
+        while(a<5)
+        {
+            System.out.println("HI");
+            a++;
+        }
+    }
+}
